da1114355a2c0deb0129e240dc9e261eea6b753d
cesarwillemberg <cesaraugustow17@hotmail.com>
2026-01-30 13:49:09 -0300

    Fix homepage URL and update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'node:path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FitMetrics/",
+  base: "/FitMetrics",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
